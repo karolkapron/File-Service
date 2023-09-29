ed360b92bf2602ee77a90f880428cd2405995195
karolkapron <karolkapron11@gmail.com>
2023-09-29 19:38:25 +0200

    add: other Crud methods to controller

--- src/main/java/com/example/FileService/Controller/FileController.java
+++ src/main/java/com/example/FileService/Controller/FileController.java
@@ -4,7 +4,6 @@ import com.example.FileService.Model.File;
 import com.example.FileService.Service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +27,24 @@ public class FileController {
     @PostMapping("/{filename}")
     public ResponseEntity<File> addNewFile(@RequestBody File newFile){
         return ResponseEntity.ok(fileService.saveFile(newFile));
+    }
+    @PutMapping("/{filename}")
+    public ResponseEntity<File> updateFile(@PathVariable String filename, @RequestBody File fileToUpdate){
+        return fileService.getFileByFilename(filename)
+                .map(existingFile -> {
+                    existingFile.setFilename(fileToUpdate.getFilename());
+                    existingFile.setFolder(fileToUpdate.getFolder());
+                    existingFile.setSizeInByte(fileToUpdate.getSizeInByte());
+                    return ResponseEntity.ok(fileService.saveFile(existingFile));
+
+                }).orElse(ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/{filename}")
+    public ResponseEntity<Object> deleteFile(@PathVariable String filename){
+        return fileService.getFileByFilename(filename)
+                .map(existingFile->{
+                    fileService.deleteFile(existingFile.getId());
+                    return ResponseEntity.noContent().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
